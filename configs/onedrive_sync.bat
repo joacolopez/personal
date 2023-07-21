b757bbf7b1722a6ee2542178e8ea45f25aabd48f
Nekrr <joacolopez94@gmail.com>
2023-07-20 22:06:40 -0300

    Delete configs directory

--- configs/onedrive_sync.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM ADD CUSTOM FOLDER TO ONE DRIVE
-set /p "onedrivefolder=One drive folder name: "
-set /p "customfolder=Path to folder: "
-
-REM set sync folder 
-mklink /j "%UserProfile%\OneDrive\%onedrivefolder%" "%customfolder%"
